5306a3033df40b7955ba1bb59ebec3b1bbc12095
yamadaminorutou <rendengshantian87@gmail.com>
2026-06-03 10:45:39 +0900

    first commit

---
+++ src/main/java/org/example/mydailymemoapp/MyDailyMemoAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.mydailymemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDailyMemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDailyMemoAppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/mydailymemoapp/UserService.java
@@ -0,0 +1,4 @@
+package org.example.mydailymemoapp;
+
+public class UserService {
+}
